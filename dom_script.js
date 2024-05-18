b16b882ab7144830cb945edcdb1c1d1dcfe9141d
Johnny Platinum <johnnyplatinum178@gmail.com>
2024-05-18 16:12:45 -0400

    implemented the ability of content scripts to extract url's from the search results of social-searcher.com

--- dom_script.js
+++ dom_script.js
@@ -1,15 +1,37 @@
-function collectDivsByClass(className) {
-    let divs = document.querySelectorAll('div.' + className);
-    let divArray = Array.from(divs);
-    return divArray;
+function collectElementsBySelector(selector) {
+    let elements = document.querySelectorAll(selector);
+    let elementsArray = Array.from(elements);
+    return elementsArray;
   }
+
+
+  function collectDOMProperties(arrayOfElements,domProperty) {     
+    var arrayOfDOMProperties = arrayOfElements.map(function(element) {
+      return element.domProperty;
+    });
+    
+    return arrayOfDOMProperties;
+  }
+
 
 
  function collectInnerTextValues(arrayOfElements) {     
     var arrayOfInnerTextValues = arrayOfElements.map(function(element) {
       return element.innerText;
-    });
-  
-    // Return the array of innerText values
+    });  
     return arrayOfInnerTextValues;
   }
+
+
+  function collectHrefValues(arrayOfAnchorElements) {     
+    var arrayOfHrefValues = arrayOfAnchorElements.map(function(anchorElement) {
+      return anchorElement.href;
+    });
+    
+    return arrayOfHrefValues;
+  }
+
+
+
+  
+
